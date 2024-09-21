1aa742839c7ca123cb5865eef1dba6d6b392b601
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-21 03:52:45 +0000

    Release 1.0.0

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+export { default as isContainerDirective } from './is-container-directive.js';
+export { default as isLeafDirective } from './is-leaf-directive.js';
+export { default as isTextDirective } from './is-text-directive.js';
